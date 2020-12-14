84b1a46e5b1e3ae226c8c846ebee7e8455c2a7c6
hyunjeong hwang <20hyun17jeong@jbnu.ac.kr>
2020-12-14 23:38:10 +0900

    added python file

---
+++ ex210.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# def로 함수를 정의해준다.
+# 출력해야 하는 코드는 message3()이다.
+
+
+# In[2]:
+
+
+# i=0일 때 message2()값을 출력하고, "C"를 출력한다.
+    # B
+    # C
+# i=1일 때 message2()값을 출력하고, "C"를 출력한다.
+    # B
+    # C
+# i=2일 때 message2()값을 출력하고, "C"를 출력한다.
+    # B
+    # C
+# message1()을 출력해준다.
+    # A
+
