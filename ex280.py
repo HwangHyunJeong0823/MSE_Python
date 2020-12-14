2c99f82c4caa4f841a26a654f4945b81da8a31e1
hyunjeong hwang <20hyun17jeong@jbnu.ac.kr>
2020-12-15 00:03:37 +0900

    added python file

---
+++ ex280.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# withdraw_history() 메서드와 deposit_history() 메서드를 사용한다.
+# 구현하기 위해 pass를 사용해준다.
+
+
+# In[2]:
+
+
+# 입금이 될 때마다 입금사항을, 출금이 될 때마다 출금사항을 기록해주어야 한다.
+# 객체가 생성될 때 deposit_log에 비어있는 리스트를 만들고 withdraw_log에 비어있는 리스트를 만들어준다.
+
+
+# In[3]:
+
+
+# 예금할 때마다 self.deposit_log.append(amount)를 리스트로 지정해준다,
+# 출금할 때도 마찬가지로 해준다.
+
+
+# In[4]:
+
+
+# 출금내역은 for ammount in self.withdraw_log: 로 지정해준다.
+# print(amount)를 해주면 출금액이 출력된다.
+#입금내역도 마찬가지로 해준다.
+
+
+# In[5]:
+
+
+# 입금할 때는 k = Account("Kim", 1000)으로 지정해준다.
+# k.deposit()으로 하고 괄호 안에 값을 넣어준다.
+# k.deposit_history()를 호출해준다.
+# 출금도 마찬가지로 해준다.
+
